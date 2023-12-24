393a4cb78c074ab04a862930418827a05ed41096
aeem <tattapan.wan@everapp.io>
2023-12-24 16:21:16 +0700

    add mongo

--- src/Controllers/testControllers.ts
+++ src/Controllers/testControllers.ts
@@ -1,38 +1,18 @@
 import express, { Application, Request, Response } from 'express';
 
-import fs from 'fs';
-import path from 'path';
-const filePath = path.join(__dirname, '../Controllers/list.json');
+
 
 
 
 exports.listtest = async (req:Request, res:Response) => {
 
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-          console.error('Error reading JSON file:', err);
-          res.status(500).json({
-            message: 'Error reading JSON file',
-          });
-          return;
-        }
-        try {
-            // แปลงข้อมูล JSON เป็น Object
-            const jsonData = JSON.parse(data);
-      
-            // ส่งข้อมูลใน response
+
+        
             res.status(200).json({
               message: 'listproducts',
-              data: jsonData,
-            });
-          } catch (jsonError) {
-            console.error('Error parsing JSON:', jsonError);
-            res.status(500).json({
-              message: 'Error parsing JSON',
-            });
-          }
-
-  })};
+              data:  'sucessData'
+            });        
+  };
 
   exports.createtest = async (req:Request, res:Response) => {
         const {name } = req.body
